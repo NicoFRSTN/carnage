df8a691f4ecb6500a19e513e335b517ea1e7c032
Nicolas Feuerstein <nicolas.feuerstein@gmail.com>
2021-08-17 15:57:42 +0200

    car controller + car new view

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,16 +5,33 @@ class CarsController < ApplicationController
   end
 
   def show
+    @car = Car.find(params[:id])
+    @booking = Booking.new
   end
 
   def new
+    @car = Car.new
   end
 
   def create
-
+    @car = Car.new(car_params)
+    @car.owner = current_user
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path
+  end
 
+  private
+
+  def car_params
+    Car.require(:car).permit(:brand, :model, :fuel, :price, :gearbox, :owner, :photo_url)
   end
 end
