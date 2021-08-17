976d9b74c00c7188cd482c0bf35fee6d996da18a
Leo GENUIT <genuitleo@gmail.com>
2021-08-17 16:48:41 +0200

    Add: BookingsControllert

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @car = Car.find(params[:car_id])
+    @booking = Booking.new(car_params)
+    @booking.customer = current_user
+    @booking.car = @car
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render 'cars/show'
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:date_start, :date_end)
+  end
+end
