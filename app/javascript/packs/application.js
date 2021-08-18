bdb5969eb72fc102af883eec1d954653dace59b7
thithuha nguyen <chloe.nguyen1910@gmail.com>
2021-08-18 17:06:59 +0200

    add to application stylesheet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,5 +35,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
-  // initAutocomplete();
+  initAutocomplete();
 });
